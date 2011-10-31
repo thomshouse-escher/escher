e0a0438400fd7b4d6ae8c2ce0894cc34361b6537
Thom Stricklin <thomshouse@gmail.com>
2011-10-31 15:01:47 -0700

    Added cron.php
    - Added /cron.php which is requied for properly running /escher/cron.php

---
+++ cron.php
@@ -0,0 +1,11 @@
+<?php
+
+// Let's keep this file as simple as possible!
+
+// Load config file
+require_once('config.php');
+
+// Initialize framework
+require_once('escher/cron.php');
+
+// That's all folks!!!
