1907622853eeef2c3945cfeae1d4a9a28d0ab75b
Andrew Detwiler <andetwiler@mpsaz.org>
2011-11-04 13:37:13 -0700

    Add dynamic router
    The dynamic router extends the functionality of the static router.
    It attempts to match elements of the path to nodes in the database
    organized by parent/child relationships.
    
    * Provides the same functions as the static router.
    * Certain functions (e.g. findRoute() and getPathByInstance())
      are extended to accommodate dynamic routes when a static route
      cannot be found.
    * New function getPathById() builds the path string based on the
      provided route id.

---
+++ escher/helpers/router/dynamic/Helper_router_dynamic.php
@@ -0,0 +1,76 @@
+<?php
+
+class Helper_router_dynamic extends Helper_router {
+
+	protected function findRoute() {
+		if ($result = parent::findRoute()) { return $result; }
+
+		$CFG = Load::CFG();
+		$route['id'] = $CFG['root']['id'];
+		$current = $parent = $site = array();
+		$args = explode('/',$this->path);
+		while (!empty($args)
+			&& $m = Load::Model('route_dynamic',array(
+				'parent_id'=>$route['id'],'tag'=>$args[0]))
+		) {
+			$model = $m;
+			$parent = $current;
+			$current[] = array_shift($args);
+
+			if (!empty($m->theme)) { $route['theme'] = $m->theme; }
+			if (!empty($m->subsite)) { $site = $current; }
+
+			$route['id'] = $m->id;
+			$route['route_ids'][] = $m->id;
+		}
+		
+		if (empty($current)) { return false; }
+
+		$route['current_path'] = implode('/',$current);
+		$route['parent_path'] = implode('/',$parent);
+		$route['site_path'] = implode('/',$site);
+		$route['controller'] = $model->controller;
+		if (!empty($model->instance_id)) { $route['instance_id'] = $model->instance_id; }
+		$route['title'] = $model->title;
+		$route['args'] = $args;
+		$route['route'] = $model;
+
+		return $route;
+	}
+
+	function getPathByInstance($controller,$id=NULL,$absolute=TRUE) {
+		$result = parent::getPathByInstance($controller,$id,$absolute);
+		if (!empty($result)) { return $result; }
+		
+		$m = Load::Model('route_dynamic',
+			array('controller'=>$controller, 'instance_id'=>$id));
+		if (empty($m)) { return false; }
+		
+		$path = array($m->tag);
+
+		while (!empty($m->parent_id)) {
+			$m = Load::Model('route_dynamic',$m->parent_id);
+			if (!empty($m->parent_id)) { array_unshift($path, $m->tag); }
+		}
+		
+		$path = implode('/',$path);
+
+		return $this->getRootPath($absolute)."/$path";
+	}
+
+	function getPathById($id,$absolute=TRUE) {
+		$m = Load::Model('route_dynamic',$id);
+		if (empty($m)) { return false; }
+		
+		$path = array($m->tag);
+
+		while (!empty($m->parent_id)) {
+			$m = Load::Model('route_dynamic',$m->parent_id);
+			if (!empty($m->parent_id)) { array_unshift($path, $m->tag); }
+		}
+		
+		$path = implode('/',$path);
+
+		return $this->getRootPath($absolute)."/$path";
+	}
+}
