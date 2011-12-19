thomshouse-escher/escher

commit 47c3c1795e1fa890c85c203bd30fb44066965e57

Changing filename convention for classes
* Changes file naming convention for controllers, models, and helpers
  (as well as some base classes).
* Example: `Controller_page.php` is renamed to `controller.page.php`.
* Helper filenames have additionaly changed in that the helper type
  and helper name are now separated by a period.
* Example: `Helper_userauth_local.php` is renamed to
  `helper.userauth.local.php`.
* Changes to EscherInit and Load to accommodate the new filenames.

escher/helpers/hooks/helper.hooks.php
@@ -9,9 +9,9 @@ abstract class Helper_hooks extends Helper {
 	protected $models = array();
 	protected $staticroutes = array();
 
-	function registerEvent($event,$funcname,$priority=0,$args=-1) {
+	function registerEvent($event,$callback,$priority=0) {
 		$priority = (int)$priority;
-		$this->events[$event][$priority][$funcname] = $args;
+		$this->events[$event][$priority][] = $callback;
 		return true;
 	}
 	
@@ -20,21 +20,24 @@ abstract class Helper_hooks extends Helper {
 		$callbacks = $this->events[$event];
 		ksort($callbacks);
 		$result = array();
-		foreach($callbacks as $pr) {
-			foreach($pr as $call => $maxargs) {
-				$arg = $args;
-				if ($maxargs>=0) {
-					$arg = array_slice($arg,0,$maxargs);
+		foreach($callbacks as $priority) {
+			foreach($priority as $callback) {
+				$callname = $callback;
+				if (is_array($callname)) {
+					if (is_object($callname[0])) {
+						$callname[0] = get_class($callname[0]);
+					}
+					$callname = implode('::',$callname);
 				}
-				$result[$call] = call_user_func_array($call,$arg);
+				$result[$callname] = call_user_func_array($callback);
 			}
 		}
 		return $result;
 	}
 
-	function registerFilter($filter,$funcname,$priority=0) {
+	function registerFilter($filter,$callback,$priority=0) {
 		$priority = (int)$priority;
-		$this->filters[$filter][$priority][] = $funcname;
+		$this->filters[$filter][$priority][] = $callback;
 		return true;
 	}
 	
@@ -43,8 +46,8 @@ abstract class Helper_hooks extends Helper {
 		$callbacks = $this->filters[$filter];
 		ksort($callbacks);
 		foreach($callbacks as $pr) {
-			foreach($pr as $call) {
-				$value = call_user_func($call,$value);
+			foreach($pr as $callback) {
+				$value = call_user_func($callback,$value);
 			}
 		}
 		return $value;
@@ -143,11 +146,11 @@ abstract class Helper_hooks extends Helper {
 		} else { return false; }
 	}
 	
-	function registerOutputFunction($varname,$funcname) {
-		if(!is_string($varname) || !is_string($funcname)) {
+	function registerOutputFunction($funcname,$realname) {
+		if(!is_string($funcname) || !is_string($funcname)) {
 			return false;
 		}
-		$this->outputFunctions[$varname] = $funcname;
+		$this->outputFunctions[$funcname] = $realname;
 		return true;
 	}
 		

commit c94bc288ce428fca2ff854582d3115c6ff71be63

Fixing bugs introduced by previous commit.
* Helper_session_datasource.php now properly renamed as per 47c3c1795e.
* Function $hook->runEvent now properly passes args.
* Fixes #26 caused by 47c3c1795e. Sorry!

escher/helpers/hooks/helper.hooks.php
@@ -29,7 +29,7 @@ abstract class Helper_hooks extends Helper {
 					}
 					$callname = implode('::',$callname);
 				}
-				$result[$callname] = call_user_func_array($callback);
+				$result[$callname] = call_user_func_array($callback,$args);
 			}
 		}
 		return $result;