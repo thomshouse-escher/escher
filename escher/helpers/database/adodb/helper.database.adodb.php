417b8710ec97e77b110140b7cb4e458fc0fec9da
Thom Stricklin <tgstrick@mpsaz.org>
2012-02-06 15:29:13 -0700

    Adding schema support to Datasource & DB helpers
    * Adds schema support to datasource.
    * Adds partial schema support to ADOdb helper.

--- escher/helpers/database/adodb/helper.database.adodb.php
+++ escher/helpers/database/adodb/helper.database.adodb.php
@@ -1,6 +1,9 @@
 <?php
 
 class Helper_database_adodb extends Helper_database {
+	protected $intTypes = array('tinyint','smallint','mediumint','int','bigint');
+	protected $stringTypes = array('char','binary','varchar','varbinary');
+	protected $contentTypes = array('tinytext','text','mediumtext','longtext');
 	var $adodb;
 	
 	function __construct($args) {
@@ -130,14 +133,128 @@ class Helper_database_adodb extends Helper_database {
 	
 	function getSchema($table) {
 		$this->connect();
-		$columns = $this->adodb->MetaColumns($this->t($table));
-		$result = array();
-		if (is_array($columns)) {
-			foreach($columns as $c) {
-				$result[$c->name] = $c->type;
+		$schema = array();
+		$partitions = array(
+			'fields'   => $this->prefix.$table,
+			'metadata' => $this->prefix.$table.'_metadata',
+			'content'  => $this->prefix.$table.'_content',
+		);
+		foreach($partitions as $n => $t) {
+			$fields = array();
+			$fraw = $this->adodb->MetaColumns($t);
+			if ($fraw) {
+				foreach($fraw as $f) {
+					$fa = array(
+						'type'           => $f->type,
+						'length'         => $f->max_length,
+						'not_null'       => $f->not_null,
+						'auto_increment' => $f->auto_increment,
+						'binary'         => $f->binary,
+						'unsigned'       => $f->unsigned,
+						'zerofill'       => $f->zerofill,
+						'default'        => $f->has_default
+							? $f->default_value
+							: NULL,
+					);
+					
+					foreach($fa as $k => $v) {
+						if(empty($v)) { unset($fa[$k]); }
+					}
+					$fields[$f->name] = $fa;
+				}
+			}
+			$schema[$n] = $fields;
+		}
+		return $schema;
+	}
+
+	function setSchema($table,$schema,$complete=FALSE) {
+		// Get the current state of the database
+		$dbSchema = $this->getSchema($table);
+
+		// Detect new fields to add to DB schema
+		$changeCols = array_diff_key($schema['fields'],$dbSchema['fields']);
+
+		// Detect fields to modify in DB schema
+		$checkCols = array_keys(
+			array_intersect_key($schema['fields'],$dbSchema['fields']));
+		foreach($checkCols as $c) {
+			$after = array_diff($schema['fields'][$c],$dbSchema['fields'][$c]);
+			if (empty($after)) { continue; }
+			if ($complete) { $changeCols[$c] = $after; continue; }
+			$before = array_intersect_key($dbSchema['fields'][$c],$after);
+			$diff = array('type' => $dbSchema['fields'][$c]['type']);
+			foreach($after as $k => $f) {
+				switch ($k) {
+					case 'type':
+						if (in_array($after[$k],$this->intTypes)
+							&& in_array($before[$k],$this->intTypes)
+							&& array_search($after[$k],$this->intTypes) >
+								array_search($before[$k],$this->intTypes)
+						) {
+							$diff[$k] = $f;
+						} elseif (in_array($after[$k],$this->contentTypes)
+							&& in_array($before[$k],$this->contentTypes)
+							&& array_search($after[$k],$this->contentTypes) >
+								array_search($before[$k],$this->contentTypes)
+						) {
+							$diff[$k] = $f;
+						}
+						break;
+					case 'length':
+						if ($f > $before['length']) {
+							$diff[$k] = $f;
+						}
+						break;
+					case 'unsigned': case 'auto_increment': break;
+					default:
+						$diff[$k] = $f; break;
+				}
+			}
+			if (!empty($diff)) { $changeCols[$c] = $diff; }
+		}
+
+		// Start building ADOdb datadict syntax per table
+		$changeTables = array(
+			'main'   => array(),
+			'metadata' => array(),
+			'content'  => array(),
+		);
+		foreach($changeCols as $name => $c) {
+			$def = "$name {$c['type']}";
+			if (!empty($c['length'])) { $def .= "({$c['length']})"; }
+			if (!empty($c['binary'])) { $def .= " BINARY"; }
+			if (!empty($c['unsigned'])) { $def .= " UNSIGNED"; }
+			if (!empty($c['zerofill'])) { $def .= " ZEROFILL"; }
+			$def .= empty($c['not_null']) ? ' NULL' : ' NOTNULL';
+			if (!empty($c['default'])) { $def .= " DEF {$this->q($c['default'])}"; }
+			if (!empty($c['metadata'])) {
+				$changeTables['metadata'][] = $def;
+			} elseif (in_array($c['type'],$this->contentTypes)) {
+				$changeTables['content'][] = $def;
+			} else {
+				$changeTables['main'][] = $def;
 			}
 		}
-		return $result;
+
+		$tableNames = array(
+			'main'   => $this->prefix.$table,
+			'metadata' => $this->prefix.$table.'_metadata',
+			'content'  => $this->prefix.$table.'_content',
+		);
+
+		$this->connect();
+		$this->db->debug = TRUE;
+		$dict = NewDataDictionary($this->db);
+		foreach($changeTables as $table => $fields) {
+			if (!empty($fields)) {
+				echo $this->db->CreateTableSQL(
+					$tableNames[$table],
+					implode(',',$fields)
+				);
+			}
+		}
+		die();
 	}
 	
 	function getOne($sql,$vars=NULL) {
@@ -159,11 +276,25 @@ class Helper_database_adodb extends Helper_database {
 		return !is_null($this->adodb);
 	}
 	
+	function n($str) {
+		$this->connect();
+		$d = $this->adodb->nameQuote;
+		return preg_replace(
+			array('/[^\w_.*0-9]/','/([\w0-9_]+)/'),
+			array('',$d.'$1'.$d),
+			$str
+		);
+	}
+
 	function q($str) {
 		$this->connect();
 		return $this->adodb->qstr($str);
 	}
-	
+
+	function t($str) {
+		return $this->n(parent::t($str));
+	}
+
 	function date($ts) {
 		$this->connect();
 		return $this->adodb->BindDate($ts);
