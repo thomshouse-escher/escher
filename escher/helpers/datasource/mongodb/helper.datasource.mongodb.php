8a816b0351cc10f39bcb1ade5f4a942716a56912
Andrew Detwiler <adetwiler@adidamnetworks.com>
2012-06-20 03:49:19 +0000

    Fixing issues in MongoDB datasource
    * Fixes setting and getting models.
    * Adds different select types (all, one, col).
    * Adds order.
    * Adds sort.

--- escher/helpers/datasource/mongodb/helper.datasource.mongodb.php
+++ escher/helpers/datasource/mongodb/helper.datasource.mongodb.php
@@ -58,7 +58,7 @@
 			$result = $this->mongodb->selectCollection($collection)->insert($data,array('safe'=>TRUE));
 			$result = empty($result['err']);
 			if ($result) {
-				return $data['_id'];
+				return $data[$collection.'_id'];
 			}
 			return false;
 		}
@@ -70,34 +70,88 @@
 			array(
 				'select' => '*',
 				'limit'  => 1,
-				'order'  => '',
+				'order'  => array(),
 				'group'  => '',
 				'joins'   => array(),
 			),
 			$options
 		);
+		if (!is_array($options['select'])) {
+			$options['select'] = explode(',',$options['select']);
+		}
+		
 		if (is_object($model)) {
 			$collection = $model->_m();
 		} elseif (is_string($model)) {
 			$collection = $model;
 		} else { return false; }
+		
+		// Get Mongo order clause
+		if (is_array($options['order'])) {
+			// Interpret array notation
+			$o=array();
+			foreach($options['order'] as $k => $v) {
+				if ($v>0) { $o[$k] = 1; }
+				elseif ($v<0) { $o[$k] = -1; }
+				else { $o[] = $k; }
+			}
+			$options['order'] = $o;
+		}
 
-		if (empty($limit)) {
-			$limit=array(0,1);
-		} elseif (!is_array($limit)) {
-			$limit=array(0,$limit);
+		if (is_array($options['limit'])) {
+			// Convert array to skip,limit
+			$skip = (int)$options['limit'][0];
+			$limit = (int)$options['limit'][1];
+			$options['limit'] = (int)$options['limit'][1];
+		} elseif ($options['limit']>0) {
+			$skip = 0;
+			$limit = (int)$options['limit'];
+		} else { 
+			$skip=0;
+			$limit = 0; 
 		}
 
 		$data = $this->mongodb->selectCollection($collection)
 			->find($conditions)
-			->skip($limit[0])
-			->limit($limit[1]);
+			->skip($skip)
+			->limit($limit)
+			->sort($options['order']);
 
 		$result=array();
 		foreach ($data as $d) {
 			unset($d['_id']);
 			$result[] = $d;
 		}
+
+		if (empty($result)) { return false; }
+
+		if (empty($options['fetch'])) {
+		    if (sizeof($options['select'])==1 && !preg_match('/[*]/',$options['select'][0])) {
+		        $options['fetch'] = $options['limit']==1 ? 'one' : 'col';
+		    } else {
+		        $options['fetch'] = $options['limit']==1 ? 'row' : 'all';
+		    }
+		}
+
+		switch ($options['fetch']) {
+		    case 'one': return reset($result[0]); break;
+		    case 'row': return $result[0]; break;
+		    case 'col':
+				if (array_key_exists(reset($options['select']),$result[0])) {
+				    $k = reset($options['select']);
+				} else {
+				    reset($result[0]);
+				    $k = key($result[0]);
+				}
+				$col = array();
+				foreach ($result as $r) {
+				   $col[] = $r[$k];
+				}
+				return $col;
+		    break;
+		    case 'all': return $result; break;
+		}
+		
 		return $result;
 	}
 
