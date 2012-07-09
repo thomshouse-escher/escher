03a7a6957cec3fcb5d2d4911a439311f3ab1ec81
Thom Stricklin <thomshouse@gmail.com>
2012-07-08 22:02:57 -0700

    Adding observer pattern to EscherObject
    - Adds observer pattern to base EscherObject class.
    - Method $foo->attachObserver($bar) adds $bar as an observer of $foo.
    - Method $foo->detachObserver($bar) removes $bar as an observer.
    - Method $foo->notifyObservers($event) notifies all current observers
      of $foo, providing the $foo object as well as optional $event type.
    - Method $foo->observeObject($bar) adds $foo as an observer of $bar.
      This method is a convenience wrapper for attachObserver, flipping
      the direction of attachment.
    - Method $foo->onObservation($bar,$event) is a placeholder function for
      handling observation events.  All observer objects should extend
      this method in order to be of any use.

--- escher/core/escher.object.php
+++ escher/core/escher.object.php
@@ -1,10 +1,10 @@
 <?php
 
 class EscherObject {
-	function __construct() {
-		
-	}
-	
+	protected $_observers = array();
+
+	function __construct() { }
+
 	// Assign an associative array of variables to this object
 	public function assignVars($vars) {
 		if (!is_array($vars) || array_values($vars)==$vars) {
@@ -15,7 +15,38 @@ class EscherObject {
 		}
 		return true;
 	}
-	
+
+	public function attachObserver($object) {
+		if (is_a($object,'EscherObject')) {
+			$this->detachObserver($object);
+			$this->_observers[] = $object;
+		}
+	}
+
+	public function detachObserver($object) {
+		if (is_a($object,'EscherObject')) {
+			foreach($this->_observers as $k => $o) {
+				if ($o===$object) {
+					unset($this->_observers[$k]);
+				}
+			}
+		}
+	}
+
+	public function notifyObservers($event=NULL) {
+		foreach($this->_observers as $o) {
+			$o->onObservation($this,$event);
+		}
+	}
+
+	public function observeObject($object) {
+		if (is_a($object,'EscherObject')) {
+			$object->attachObserver($this);
+		}
+	}
+
+	public function onObservation($object,$event=NULL) { }
+
 	// What plugin (if any) does this object come from?
 	public function _plugin() {
 		if ($this) { $class = get_class($this); }
