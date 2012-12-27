b142edc5620c5e21c3bd25ecb9cfea5c2c32b6bb
Thom Stricklin <thomshouse@gmail.com>
2012-12-27 01:12:21 -0700

    Fixing bug with router dispatches
    * Fixes a bug caused where dispatches would create a loop between
      router objects.
    * Improves chainability of dispatch routers.

--- escher/helpers/router/dispatch/helper.router.dispatch.php
+++ escher/helpers/router/dispatch/helper.router.dispatch.php
@@ -1,76 +1,96 @@
 <?php
 
 class Helper_router_dispatch extends Helper_router {
-	protected $sourceRouter;
-	protected $dispatch;
-	protected $base = '';
-	protected $args = array();
+    protected $sourceRouter;
+    protected $dispatch;
+    protected $base = '';
+    protected $args = array();
+    protected $defer = FALSE;
 
-	function __construct($args) {
-		if (!is_array($args)) {
-			throw new ErrorException(
-				'Constructor arguments should be in the form of an array');
-		}
-		if (!isset($args['router']) || !is_a($args['router'],'Helper_router')) {
-			throw new ErrorException(
-				'Helper_router_dispatch requires a Helper_router object.'
-			);
-		}
-		if (!isset($args['dispatch'])) {
-			throw new ErrorException(
-				'Helper_router_dispatch requires the name of the dispatch'
-			);
-		}
-		$this->sourceRouter = $args['router'];
-		$this->dispatch = $args['dispatch'];
-		if (isset($args['base'])) {
-			$this->base = $args['base'];
-		}
-		if (isset($args['args'])) {
-			$this->args = $args['args'];
-		}
-	}
+    function __construct($args) {
+        if (!is_array($args)) {
+            throw new ErrorException(
+                'Constructor arguments should be in the form of an array');
+        }
+        if (!isset($args['router']) || !is_a($args['router'],'Helper_router')) {
+            throw new ErrorException(
+                'Helper_router_dispatch requires a Helper_router object.'
+            );
+        }
+        if (!isset($args['dispatch'])) {
+            throw new ErrorException(
+                'Helper_router_dispatch requires the name of the dispatch'
+            );
+        }
+        $this->sourceRouter = $args['router'];
+        $this->dispatch = $args['dispatch'];
+        if (isset($args['base'])) {
+            $this->base = $args['base'];
+        }
+        if (isset($args['args'])) {
+            $this->args = $args['args'];
+        }
+    }
 
-	function getRoute() { return $this->sourceRouter->getRoute(); }
-	function getContext() { return $this->sourceRouter->getRoute(); }
+    function getRoute() { return $this->sourceRouter->getRoute(); }
+    function getContext() { return $this->sourceRouter->getRoute(); }
 
-	function getCurrentPath($absolute=TRUE,$args=FALSE,$qsa=FALSE) {
-		$path = $this->getParentPath($absolute);
-		if (!empty($this->base)) { $path .= '/'.$this->base; }
-		if ($args && !empty($this->args)) {
-			$path .= '/'.implode('/',$this->args);
-			if ($qsa && !empty($_SERVER['QUERY_STRING'])) {
-				$current.= '?'.$_SERVER['QUERY_STRING'];
-			}
-		}
-		return $path;
-	}
+    function getCurrentPath($absolute=TRUE,$args=FALSE,$qsa=FALSE) {
+        $path = $this->getParentPath($absolute);
+        if (!empty($this->base)) { $path .= '/'.$this->base; }
+        if ($args && !empty($this->args)) {
+            $path .= '/'.implode('/',$this->args);
+            if ($qsa && !empty($_SERVER['QUERY_STRING'])) {
+                $current.= '?'.$_SERVER['QUERY_STRING'];
+            }
+        }
+        return $path;
+    }
 
-	function getParentPath($absolute=TRUE) {
-		return $this->sourceRouter->getCurrentPath($absolute);
-	}
+    function getParentPath($absolute=TRUE) {
+        $this->defer = TRUE;
+        $result = $this->sourceRouter->getCurrentPath($absolute);
+        $this->defer = FALSE;
+        return $result;
+    }
 
-	function getSitePath($absolute=TRUE) {
-		return $this->sourceRouter->getSitePath($absolute);
-	}
+    function getSitePath($absolute=TRUE) {
+        $this->defer = TRUE;
+        $result = $this->sourceRouter->getSitePath($absolute);
+        $this->defer = FALSE;
+        return $result;
+    }
 
-	function getPathByInstance($controller,$id=NULL,$absolute=TRUE) {
-		return $this->sourceRouter->getPathByInstance($controller,$id,$absolute);
-	}
+    function getPathByInstance($controller,$id=NULL,$absolute=TRUE) {
+        $this->defer = TRUE;
+        $result = $this->sourceRouter->getPathByInstance($controller,$id,$absolute);
+        $this->defer = FALSE;
+        return $result;
+    }
 
-	function getPathById($id,$absolute=TRUE) {
-		if (method_exists($this->sourceRouter,'getPathById')) {
-			return $this->sourceRouter->getPathById($id,$absolute);
-		} else {
-			return false;
-		}
-	}
+    function getPathById($id,$absolute=TRUE) {
+        if (method_exists($this->sourceRouter,'getPathById')) {
+            $this->defer = TRUE;
+            $result = $this->sourceRouter->getPathById($id,$absolute);
+            $this->defer = FALSE;
+            return $result;
+        } else {
+            return false;
+        }
+    }
 
-	function resolvePath($url,$absolute=TRUE) {
-		if (preg_match('#^\.\./\.\.(/.*|)#',$url,$match)) {
-			return $this->sourceRouter->getParentPath($absolute).$match[1];
-		} else {
-			return parent::resolvePath($url,$absolute);
-		}
-	}
+    function resolvePath($url,$absolute=TRUE) {
+        if (preg_match('#^\.\./\.\.(/.*|)#',$url,$match)) {
+            $this->defer = TRUE;
+            $result = $this->sourceRouter->getParentPath($absolute).$match[1];
+            $this->defer = FALSE;
+            return $result;
+        } else {
+            return parent::resolvePath($url,$absolute);
+        }
+    }
+
+    function deferringToSource() {
+        return $this->defer;
+    }
 }
--- escher/helpers/router/helper.router.php
+++ escher/helpers/router/helper.router.php
@@ -3,186 +3,186 @@
 abstract class Helper_router extends Helper {
     protected $dispatchRouters = array();
 
-	function __construct($args='') {
-		// If we only receive one argument and it is scalar, force it to array form
-		if (is_scalar($args)) {
-			// ...and assume the scalar value is our path
-			$args = array('path' => $args);	
-		}
-		parent::__construct($args);
+    function __construct($args='') {
+        // If we only receive one argument and it is scalar, force it to array form
+        if (is_scalar($args)) {
+            // ...and assume the scalar value is our path
+            $args = array('path' => $args);
+        }
+        parent::__construct($args);
 
-		// Get the root from the config
-		$CFG = Load::CFG();
-		$root = $CFG['root'];
+        // Get the root from the config
+        $CFG = Load::CFG();
+        $root = $CFG['root'];
 
-		// Maintenance mode changes the routes that get loaded
-		if (!empty($CFG['maintenance_mode'])) {
-			// ...but not if we have special permissions
-			$acl = Load::ACL();
-			// We have to load the route model to prevent a loop
-			$context = Load::Model('route_static','/');
-			if (!$acl->req('all',array('maintenance_mode','sysadmin'),$context)) {
-				$root = $CFG['maintenance_root'];
-				$this->static_routes = $CFG['maintenance_routes'];
-			}
-		} // End maintenance mode
+        // Maintenance mode changes the routes that get loaded
+        if (!empty($CFG['maintenance_mode'])) {
+            // ...but not if we have special permissions
+            $acl = Load::ACL();
+            // We have to load the route model to prevent a loop
+            $context = Load::Model('route_static','/');
+            if (!$acl->req('all',array('maintenance_mode','sysadmin'),$context)) {
+                $root = $CFG['maintenance_root'];
+                $this->static_routes = $CFG['maintenance_routes'];
+            }
+        } // End maintenance mode
 
-		// Attempt to find a route based on the path
-		$route = $this->findRoute();
-		// If we can't find a route, use the root/config values
-		if (!$route) {
-			$args = preg_split('#/#',$this->path,-1,PREG_SPLIT_NO_EMPTY);
-			$route = $root;
-			$route['current_path'] = '';
-			if (isset($route['args']) && is_array($route['args'])) {
-				$route['args'] = array_merge($route['args'],$args);
-			} else {
-				$route['args'] = $args;
-			}
-			$route['route'] = Load::Model('route_static','/');
+        // Attempt to find a route based on the path
+        $route = $this->findRoute();
+        // If we can't find a route, use the root/config values
+        if (!$route) {
+            $args = preg_split('#/#',$this->path,-1,PREG_SPLIT_NO_EMPTY);
+            $route = $root;
+            $route['current_path'] = '';
+            if (isset($route['args']) && is_array($route['args'])) {
+                $route['args'] = array_merge($route['args'],$args);
+            } else {
+                $route['args'] = $args;
+            }
+            $route['route'] = Load::Model('route_static','/');
             $route['theme'] = $CFG['theme'];
-		}
-		$this->assignVars($route);
-	}
+        }
+        $this->assignVars($route);
+    }
 
-	function getArgs() {
-		if (!empty($this->args)) {
-			return $this->args;
-		}
-		return array();
-	}
+    function getArgs() {
+        if (!empty($this->args)) {
+            return $this->args;
+        }
+        return array();
+    }
 
-	function getCurrentPath($absolute=TRUE,$args=FALSE,$qsa=FALSE) {
-        if (!empty($this->dispatchRouters)) {
-            return $this->dispatchRouters[0]->getCurrentPath($absolute,$args,$qsa);
+    function getCurrentPath($absolute=TRUE,$args=FALSE,$qsa=FALSE) {
+        if ($dispatch = $this->getDispatchRouter()) {
+            return $dispatch->getCurrentPath($absolute,$args,$qsa);
         }
         if ($absolute) {
-			$CFG = Load::CFG();
-			$root = $CFG['wwwroot'];
-		} else {
-			$root = '';
-		}
-		$current = $root;
-		if (!empty($this->current_path)) {
-			$current .= '/'.$this->current_path;
-		}
-		if ($args) {
-			$current .= '/'.implode('/',$this->args);
-			if ($qsa && !empty($_SERVER['QUERY_STRING'])) {
-				$current.= '?'.$_SERVER['QUERY_STRING'];
-			}
-		}
-		return $current;
-	}
-
-	function getParentPath($absolute=TRUE) {
-        if (!empty($this->dispatchRouters)) {
-            return $this->dispatchRouters[0]->getParentPath($absolute);
+            $CFG = Load::CFG();
+            $root = $CFG['wwwroot'];
+        } else {
+            $root = '';
         }
-		if ($absolute) {
-			$CFG = Load::CFG();
-			$root = $CFG['wwwroot'];
-		} else {
-			$root = '';
-		}
-		$parent = $root;
-		if(!empty($this->parent_path)) {
-			$parent .= '/'.$this->parent_path;
-		}
-		return $parent;
-	}
-
-	function getRootPath($absolute=TRUE) {
-		if ($absolute) {
-			$CFG = Load::CFG();
-			return $CFG['wwwroot'];
-		} else {
-			return '';
-		}
-	}
-
-	function getSitePath($absolute=TRUE) {
-		if (!empty($this->site_path) && strpos($this->site_path,'~')===FALSE) {
-			return $this->getRootPath($absolute).'/'.$this->site_path;
-		} else {
-			return $this->getRootPath($absolute);
-		}
-	}
-
-	function getPathByInstance($controller,$id=NULL,$absolute=TRUE) {
-		if (empty($controller)) { return false; }
-		if (!empty($id) && !is_numeric($id)) {
-			$action = $id; $id = NULL;
-		} else { $action = NULL; };
-		$routes = $routes = $this->getStaticRoutes();
-		foreach ($routes as $key => $route) {
-			if ($controller == $route['controller']) {
-				if (preg_match('/(\[[^]]+\]|\*)/',$key)) { continue; }
-				if ((!empty($action) || !empty($route['action']))
-					&& (empty($action) || empty($route['action'])
-						|| $action!=$route['action']
-					)
-				) { continue; }
-				if ((!empty($id) || !empty($route['instance_id']))
-					&& (empty($id) || empty($route['instance_id'])
-						|| $id!=$route['instance_id']
-					)
-				) { continue; }
-				return $this->getRootPath($absolute)."/$key";
-			}
-		}
-		return false;
-	}
-
-	function getRoute() {
-		return $this->route;
-	}
-
-	function resolvePath($url,$absolute=TRUE) {
-        if (!empty($this->dispatchRouters)) {
-            return $this->dispatchRouters[0]->resolvePath($url,$absolute);
+        $current = $root;
+        if (!empty($this->current_path)) {
+            $current .= '/'.$this->current_path;
         }
-		if (preg_match('/^#/',$url)) {
-			return $url;
-		} elseif ($absolute && preg_match('#^(/.*)#',$url,$match)) {
-			return $this->getRootPath().$match[1];
-		} elseif (preg_match('#^~(/.*|)#',$url,$match)) {
-			return $this->getSitePath($absolute).$match[1];
-		} elseif (preg_match('#^\.\.(/.*|)#',$url,$match)) {
-			return $this->getParentPath($absolute).$match[1];
-		} elseif (preg_match('#^\.(/.*|)#',$url,$match)) {
-			return $this->getCurrentPath($absolute).$match[1];
-		} elseif (preg_match('#^@(\w+)(?:\:(\w+)|)(?:>(\w+|\d+)|)(/.*|)#',$url,$match)) {
-			$controller = !empty($match[2]) ? array($match[1],$match[2]) : $match[1];
-			$args = $match[4];
-			$result = $this->getPathByInstance($controller,$match[3],$absolute);
-			if (empty($result) && !empty($match[3])) {
-				$result = $this->getPathByInstance($controller,NULL,$absolute);
-				$args = '/'.$match[3].$args;
-			}
-			if (empty($result)) { return false; }
-			return $result.$args;
-		} elseif (strpos($url,':')===FALSE) {
-			return $this->getCurrentPath($absolute)."/$url";
-		} else {
-			return $url;
-		}
-	}
-	
-	function getContext() {
-		return $this->route;
-	}
+        if ($args) {
+            $current .= '/'.implode('/',$this->args);
+            if ($qsa && !empty($_SERVER['QUERY_STRING'])) {
+                $current.= '?'.$_SERVER['QUERY_STRING'];
+            }
+        }
+        return $current;
+    }
 
-	function getController() {
-		if (empty($this->controller)) { Load::Error('404'); }
-		$args = !empty($this->args) ? $this->args : NULL;
-		$controller = Load::Controller($this->controller,$args);
-		$vars = array('router' => $this);
-		if (isset($this->action)) { $vars['action'] = $this->action; }
-		if (isset($this->instance_id)) { $vars['id'] = $this->instance_id; }
-		$controller->initialize($vars);
-		return $controller;
-	}
+    function getParentPath($absolute=TRUE) {
+        if ($dispatch = $this->getDispatchRouter()) {
+            return $dispatch->getParentPath($absolute);
+        }
+        if ($absolute) {
+            $CFG = Load::CFG();
+            $root = $CFG['wwwroot'];
+        } else {
+            $root = '';
+        }
+        $parent = $root;
+        if(!empty($this->parent_path)) {
+            $parent .= '/'.$this->parent_path;
+        }
+        return $parent;
+    }
+
+    function getRootPath($absolute=TRUE) {
+        if ($absolute) {
+            $CFG = Load::CFG();
+            return $CFG['wwwroot'];
+        } else {
+            return '';
+        }
+    }
+
+    function getSitePath($absolute=TRUE) {
+        if (!empty($this->site_path) && strpos($this->site_path,'~')===FALSE) {
+            return $this->getRootPath($absolute).'/'.$this->site_path;
+        } else {
+            return $this->getRootPath($absolute);
+        }
+    }
+
+    function getPathByInstance($controller,$id=NULL,$absolute=TRUE) {
+        if (empty($controller)) { return false; }
+        if (!empty($id) && !is_numeric($id)) {
+            $action = $id; $id = NULL;
+        } else { $action = NULL; };
+        $routes = $routes = $this->getStaticRoutes();
+        foreach ($routes as $key => $route) {
+            if ($controller == $route['controller']) {
+                if (preg_match('/(\[[^]]+\]|\*)/',$key)) { continue; }
+                if ((!empty($action) || !empty($route['action']))
+                    && (empty($action) || empty($route['action'])
+                        || $action!=$route['action']
+                    )
+                ) { continue; }
+                if ((!empty($id) || !empty($route['instance_id']))
+                    && (empty($id) || empty($route['instance_id'])
+                        || $id!=$route['instance_id']
+                    )
+                ) { continue; }
+                return $this->getRootPath($absolute)."/$key";
+            }
+        }
+        return false;
+    }
+
+    function getRoute() {
+        return $this->route;
+    }
+
+    function resolvePath($url,$absolute=TRUE) {
+        if ($dispatch = $this->getDispatchRouter()) {
+            return $dispatch->resolvePath($url,$absolute);
+        }
+        if (preg_match('/^#/',$url)) {
+            return $url;
+        } elseif ($absolute && preg_match('#^(/.*)#',$url,$match)) {
+            return $this->getRootPath().$match[1];
+        } elseif (preg_match('#^~(/.*|)#',$url,$match)) {
+            return $this->getSitePath($absolute).$match[1];
+        } elseif (preg_match('#^\.\.(/.*|)#',$url,$match)) {
+            return $this->getParentPath($absolute).$match[1];
+        } elseif (preg_match('#^\.(/.*|)#',$url,$match)) {
+            return $this->getCurrentPath($absolute).$match[1];
+        } elseif (preg_match('#^@(\w+)(?:\:(\w+)|)(?:>(\w+|\d+)|)(/.*|)#',$url,$match)) {
+            $controller = !empty($match[2]) ? array($match[1],$match[2]) : $match[1];
+            $args = $match[4];
+            $result = $this->getPathByInstance($controller,$match[3],$absolute);
+            if (empty($result) && !empty($match[3])) {
+                $result = $this->getPathByInstance($controller,NULL,$absolute);
+                $args = '/'.$match[3].$args;
+            }
+            if (empty($result)) { return false; }
+            return $result.$args;
+        } elseif (strpos($url,':')===FALSE) {
+            return $this->getCurrentPath($absolute)."/$url";
+        } else {
+            return $url;
+        }
+    }
+
+    function getContext() {
+        return $this->route;
+    }
+
+    function getController() {
+        if (empty($this->controller)) { Load::Error('404'); }
+        $args = !empty($this->args) ? $this->args : NULL;
+        $controller = Load::Controller($this->controller,$args);
+        $vars = array('router' => $this);
+        if (isset($this->action)) { $vars['action'] = $this->action; }
+        if (isset($this->instance_id)) { $vars['id'] = $this->instance_id; }
+        $controller->initialize($vars);
+        return $controller;
+    }
 
     function pushDispatchRouter($router) {
         if (is_a($router,'Helper_router_dispatch')) {
@@ -194,55 +194,64 @@ abstract class Helper_router extends Helper {
         return is_null(array_shift($this->dispatchRouters));
     }
 
-	protected function findRoute() {
-		$routes = $this->getStaticRoutes();
-		$route = array();
-		if (!empty($this->path)) {
-			foreach($routes as $key => $route) {
-				$rpreg = preg_replace(
-					array('#/?\[(num|id)\]/?#','#/?\[word\]/?#','#/?\[tag\]/?#','#/\*/?$#','#\*/?#','#/$#','#^/#'),
-					array('/(\d+)/','/([a-zA-Z]+)/','/([\w-]+)/','(/.+)?','([^\/])/','',''),$key);
-				if (preg_match("@^/?{$rpreg}(/.*|)$@",$this->path,$matches)) {
-					$route['pattern'] = $key;
-					break;
-				}
-			}
-		}
-		if (empty($matches)) {
-			return false;
-		}
-		if (!isset($route['current_path'])) {
-			$pattr = preg_split('#/#',preg_replace('#/((\[[^\]]\]|\*)/?)$#','/',
-				$route['pattern']),-1,PREG_SPLIT_NO_EMPTY);
-			$pathr = preg_split('#/#',$this->path,-1,PREG_SPLIT_NO_EMPTY);
-			$route['current_path'] = implode('/',array_slice($pathr,0,sizeof($pattr)));
-		}
-		if (!isset($route['parent_path'])) {
-			$route['parent_path'] = implode('/',array_slice(explode('/',$route['current_path']),0,-1));
-		}
+    protected function getDispatchRouter() {
+        foreach($this->dispatchRouters as $r) {
+            if (!$r->deferringToSource()) {
+                return $r;
+            }
+        }
+        return FALSE;
+    }
 
-		array_shift($matches);
-		if (empty($matches[0])) { array_shift($matches); }
-		if (!empty($matches)) {
-			if (strpos($matches[sizeof($matches)-1],'/')!==FALSE) {
-				$remaining_args = array_pop($matches);
-				$remaining_args = preg_split('#/#',$remaining_args,-1,PREG_SPLIT_NO_EMPTY);
-				$matches = array_merge($matches,$remaining_args);
-			}
-		}
-		$route['args'] = $matches;
-		$route['route'] = Load::Model('route_static','/'.$route['current_path']);
-		return $route;
-	}
+    protected function findRoute() {
+        $routes = $this->getStaticRoutes();
+        $route = array();
+        if (!empty($this->path)) {
+            foreach($routes as $key => $route) {
+                $rpreg = preg_replace(
+                    array('#/?\[(num|id)\]/?#','#/?\[word\]/?#','#/?\[tag\]/?#','#/\*/?$#','#\*/?#','#/$#','#^/#'),
+                    array('/(\d+)/','/([a-zA-Z]+)/','/([\w-]+)/','(/.+)?','([^\/])/','',''),$key);
+                if (preg_match("@^/?{$rpreg}(/.*|)$@",$this->path,$matches)) {
+                    $route['pattern'] = $key;
+                    break;
+                }
+            }
+        }
+        if (empty($matches)) {
+            return false;
+        }
+        if (!isset($route['current_path'])) {
+            $pattr = preg_split('#/#',preg_replace('#/((\[[^\]]\]|\*)/?)$#','/',
+                $route['pattern']),-1,PREG_SPLIT_NO_EMPTY);
+            $pathr = preg_split('#/#',$this->path,-1,PREG_SPLIT_NO_EMPTY);
+            $route['current_path'] = implode('/',array_slice($pathr,0,sizeof($pattr)));
+        }
+        if (!isset($route['parent_path'])) {
+            $route['parent_path'] = implode('/',array_slice(explode('/',$route['current_path']),0,-1));
+        }
 
-	protected function getStaticRoutes() {
-		$CFG = Load::CFG();
-		$routes = (array)@$this->static_routes;
-		if (empty($routes)) {
-			$hooks = Load::Hooks();
-			$routes = array_merge($CFG['static_routes'],$hooks->getStaticRoutes());
-		}
-		krsort($routes);
-		return $routes;
-	}
+        array_shift($matches);
+        if (empty($matches[0])) { array_shift($matches); }
+        if (!empty($matches)) {
+            if (strpos($matches[sizeof($matches)-1],'/')!==FALSE) {
+                $remaining_args = array_pop($matches);
+                $remaining_args = preg_split('#/#',$remaining_args,-1,PREG_SPLIT_NO_EMPTY);
+                $matches = array_merge($matches,$remaining_args);
+            }
+        }
+        $route['args'] = $matches;
+        $route['route'] = Load::Model('route_static','/'.$route['current_path']);
+        return $route;
+    }
+
+    protected function getStaticRoutes() {
+        $CFG = Load::CFG();
+        $routes = (array)@$this->static_routes;
+        if (empty($routes)) {
+            $hooks = Load::Hooks();
+            $routes = array_merge($CFG['static_routes'],$hooks->getStaticRoutes());
+        }
+        krsort($routes);
+        return $routes;
+    }
 }
